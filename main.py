2503e7fd9b8f906a0418381ddf8782f96db93d74
paceockerman <paceockerman1010@gmail.com>
2025-02-26 16:44:09 -0500

    Added a good-word storage system

--- main.py
+++ main.py
@@ -2,18 +2,14 @@
 import lang_def
 from feature import *
 from word import Word
-import jsonpickle
 # TODO: make UI faster using keystrokes instead of input()
 from pynput import keyboard
 
 
-# features = create_features(lang_def.features)
-# write_features(features)
-# features = load_features()
-
-
 # * -> Word()
-def gen_word(features):
+def gen_word(features, extant_words):
+    # TODO: have features be derived from the words given to the program
+    #   every time a new feature is gotten from a word, add it to the feature list
     syllable_count = random.randint(1, 4)
     word = Word("", [])
     for _ in range(syllable_count):
@@ -27,17 +23,20 @@ def gen_word(features):
             vowel = random_feature(lang_def.vowels, features)
             word.append(vowel)
             word.add_features(['V', vowel])
-    # TODO: consider making the features a *set* that that structures dont jump it a bunch?
+
+    # Generate a new one if this one has already been made
+    if word.text in extant_words:
+        return gen_word(features, extant_words)
+    # Consider making the features a *set* that that structures don't jump it a bunch?
+    #   ie there are 3 'CV' tags in a CVCVCV word, which may or may not be a good feature
     return word
 
-def update_weights(word, features):
+def update_weights(word, features, word_file):
     res = input(str(word)+': ')
     if res == '2':
         for feature in word.features:
             features[feature].current_weight += features[feature].change_rate
-        # TODO: make this file customizable
-        with open("good_words.txt", 'a', encoding='utf-8') as f:
-            f.write(str(word) + "\n")
+        word_file.write(str(word) + "\n")
     if res == '1':
         for feature in word.features:
             # Prevent weight from going below 1, so it always has representation
@@ -49,14 +48,19 @@ def update_weights(word, features):
 
 def main():
     # TODO have options on how to get features
-
     # features = load_features()
     features = create_features(lang_def.features)
+    # TODO: make this file name customizable
+    word_file = open('good_words.txt', 'a', encoding='utf-8')
+    extant_words = []
+    with open('good_words.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            extant_words.append(line.strip())
+    print(extant_words)
     is_done = False
     while not is_done:
-        is_done = update_weights(gen_word(features), features)
+        is_done = update_weights(gen_word(features, extant_words), features, word_file)
 
     write_features(features)
-    pass
 
 main()
